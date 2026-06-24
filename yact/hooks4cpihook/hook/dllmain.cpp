e3fe318807d3b5815956f9d7a1a9242b031cf243
Kristibek <bektashikristi@gmail.com>
2026-06-24 09:24:11 +0300

    Merge in hooks4cpihook.7z
    Taken from https://github.com/hiroki-repo/x86node-SP1-zip/commit/61ddb8d3ec3d7142bbe0df5e3ccace521198c068

---
+++ yact/hooks4cpihook/hook/dllmain.cpp
@@ -0,0 +1,27 @@
+﻿// dllmain.cpp : DLL アプリケーションのエントリ ポイントを定義します。
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        LoadLibraryA("C:\\x86node\\Windows\\cpihook.dll");
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+LRESULT WINAPI HookProc(int code, WPARAM wParam, LPARAM lParam) {
+    return NULL;
+}
+
+extern "C" __declspec(dllexport) void Inject() {
+    SetWindowsHookEx(WH_CALLWNDPROC, HookProc, GetModuleHandle(__TEXT("hook")), 0);
+}
